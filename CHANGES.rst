9f0fa5fdce95730ee49fe653dc52520b6e99c1dd
Alessandro Cifani <alessandro.cifani@gmail.com>
2017-09-29 12:13:23 +0200

    Add setup.py

---
+++ CHANGES.rst
@@ -0,0 +1,7 @@
+Changes
+=========
+
+All notable changes to this project will be documented in this file.
+
+0.1.0 - 2017-09-29
+- First release
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='Flask-SQLA2api',
+    version='0.1',
+    url='https://github.com/acifani/flask-sqla2api',
+    license='BSD',
+    author='Alessandro Cifani',
+    author_email='alessandro.cifani@gmail.com',
+    description='Flask-SQLAlchemy Model to API in one line of code',
+    packages=['flask_sqla2api'],
+    install_requires=[
+        'Flask>=0.12.2',
+        'Flask-SQLAlchemy>=2.2'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python'
+    ],
+    extras_require={
+        'test': ['pytest']
+    }
+)
