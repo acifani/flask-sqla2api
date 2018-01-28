4b710cc1605e8e2a56082ed7fa818b772759dd40
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-01-28 14:00:05 +0100

    Improve JSON support

--- flask_sqla2api/models.py
+++ flask_sqla2api/models.py
@@ -26,13 +26,15 @@ class Model(object):
     def make_blueprint(self):
         if self.model is None or self.db is None:
             raise ValueError("Model or db not initiated.")
-
         blueprint = Blueprint(self.alias, __name__)
 
         @blueprint.route(self.alias, methods=['GET', 'POST'])
         def collection_methods():
             if request.method == 'POST':
-                return self.post(request.form)
+                data = self.get_data(request)
+                if data is None:
+                    return "Unsupported media type", 415
+                return self.post(data)
             else:
                 return self.get_all()
 
@@ -42,16 +44,19 @@ class Model(object):
             if request.method == 'GET':
                 return self.get(entry)
             elif request.method == 'PUT':
-                return self.put(request.form, entry)
+                data = self.get_data(request)
+                if data is None:
+                    return "Unsupported media type", 415
+                return self.put(data, entry)
             else:
                 return self.delete(entry)
 
         return blueprint
 
-    def post(self, form):
+    def post(self, data):
         params = {}
         for field in self.fields:
-            params[field] = self.cast(field, form.get(field))
+            params[field] = self.cast(field, data[field])
         new_item = self.model(**params)
         # TODO: add try/except
         self.db.session.add(new_item)
@@ -80,10 +85,10 @@ class Model(object):
         response = jsonify(result)
         return response, 200
 
-    def put(self, form, entry):
+    def put(self, data, entry):
         result = {}
         for field in self.fields:
-            value = self.cast(field, form[field])
+            value = self.cast(field, data[field])
             setattr(entry, field, value)
             result[field] = value
         self.db.session.commit()
@@ -99,4 +104,13 @@ class Model(object):
     def cast(self, field, value):
         if not self.fields_type:
             raise ValueError("Model not initialized")
+        if value is None:
+            raise ValueError("Expected value but found None for field: %s" % field)
         return (self.fields_type[field])(value)
+
+    def get_data(self, request):
+        if request.content_type == "multipart/form-data":
+            return request.form.to_dict()
+        if request.content_type == "application/json":
+            return request.get_json()
+        return
