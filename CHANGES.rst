54b0d7f73bc5f71dd25132448997afe3d60d9728
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-02-20 20:21:16 +0100

    Bump version

--- CHANGES.rst
+++ CHANGES.rst
@@ -3,6 +3,10 @@ Changes
 
 All notable changes to this project will be documented in this file.
 
+0.2.1 - 2018-02-20
+------------------
+- Better support for JSON input and output
+
 0.2.0 - 2018-01-27
 ------------------
 - Add multiple model support
--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+import io
 from setuptools import setup
+
+
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
 
 setup(
     name='Flask-SQLA2api',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/acifani/flask-sqla2api',
     license='BSD',
     author='Alessandro Cifani',
     author_email='alessandro.cifani@gmail.com',
     description='Flask-SQLAlchemy Model to API in one line of code',
+    long_description=readme,
     packages=['flask_sqla2api'],
     install_requires=[
         'Flask>=0.12.2',
